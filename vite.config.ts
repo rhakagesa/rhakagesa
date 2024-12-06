71e53d89440bcd98d0addf6f07a6cd58bfd6fb25
rhakagesa <rhakagesa@gmail.com>
2024-12-06 21:01:42 +0700

    change resource

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
-});
+  plugins: [react()],
+})
