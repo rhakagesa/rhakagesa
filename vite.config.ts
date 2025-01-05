d69b5258529ccc24ec441ad4e832dd9bba3f840c
rhakagesa <rhakagesa@gmail.com>
2025-01-06 00:07:32 +0700

    deploy on gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "proccess.env": {
+      VITE_EMAILJS_PUBLIC_KEY: process.env.EMAILJS_PUBLIC_KEY,
+      VITE_EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
+      VITE_EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
+    },
+  },
+});
