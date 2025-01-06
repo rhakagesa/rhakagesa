3bae06bb6e6e8fdf1e49d585382bad439bd03004
rhakagesa <rhakagesa@gmail.com>
2025-01-06 17:12:15 +0700

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/rhakagesa/",
-  build: {
-    outDir: "dist",
-  },
   plugins: [react()],
   define: {
     "proccess.env": {
